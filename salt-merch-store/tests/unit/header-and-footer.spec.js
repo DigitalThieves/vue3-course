73d06197ef8ef3e04f0f5c6cd94a9b2497634ccb
Alexander Sopov <alexander@alexs-MacBook-Pro.local>
2022-10-23 20:59:16 +0200

    day-4 completely done, with tests and all

--- salt-merch-store/tests/unit/header-and-footer.spec.js
+++ salt-merch-store/tests/unit/header-and-footer.spec.js
@@ -1,5 +1,3 @@
-import App from '@/App'
-import { mount } from '@vue/test-utils'
 import router from '@/router'
 
 jest.mock('@/api-client')
@@ -9,7 +7,7 @@ beforeEach(() => {
 
 
 describe('Testing Router File for Collection', () => {
-  it.only('Should be a Vue Plugin with category route', () => {
+  it('Should be a Vue Plugin with category route', () => {
     expect(router).toBeTruthy()
     expect(router.currentRoute).toBeTruthy()
     expect(
@@ -21,19 +19,15 @@ describe('Testing Router File for Collection', () => {
     expect(
       router.options.routes.find(el => el.path.startsWith('/')).components.header
     ).toBeTruthy()
-  })
-})
-
-describe('Testing Products Page', () => {
-  it('Calls getAllProducts and displays correct amount of products', async () => {
-    client.getAllProducts.mockResolvedValueOnce(products)
-    const wrapper = mount(App, {
-      global: {
-        plugins: [router],
-      }
-    })
-    await flushPromises()
-    const title = wrapper.findAll('.row > .col-3.text-decoration-none')
-    expect(title).toHaveLength(products.length)
+    expect(
+      router.options.routes.find(el => el.path.startsWith('/about')).components.header
+    ).toBeTruthy()
+    expect(
+      router.options.routes.find(el => el.path.startsWith('/about')).components.header
+    ).toBeTruthy()
+    console.log(router.options.routes.find(el => el.path.startsWith('/about')).components.header.render())
+    expect(
+      router.options.routes.find(el => el.path.startsWith('/about')).components.header
+    ).toBeTruthy()
   })
 })
