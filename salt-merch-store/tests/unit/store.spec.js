39fac1d64d488d6799c587f3515f9b1b9537b7a2
Alexander Sopov <alexander@alexs-MacBook-Pro.local>
2022-10-28 13:23:28 +0200

    added more tests to store

--- salt-merch-store/tests/unit/store.spec.js
+++ salt-merch-store/tests/unit/store.spec.js
@@ -15,4 +15,20 @@ describe('Testing that store does what its suppose to do', () => {
     expect(store.state.cart.length).toEqual(1)
     expect(store.state.cart[0].quantity).toEqual(2)
   })
+  test('that cart has two products with quantity 2 and 1 upon dispatch of new product', async () => {
+    await store.dispatch('addItem', products[3])
+    expect(store.state.cart.length).toEqual(2)
+    expect(store.state.cart.find(el => el.slug === products[2].slug).quantity).toEqual(2)
+    expect(store.state.cart.find(el => el.slug === products[3].slug).quantity).toEqual(1)
+  })
+  test('that cart has one product with quantity 2 upon dispatch of removal of second product', async () => {
+    await store.dispatch('removeItem', store.state.cart.find(el => el.slug === products[3].slug))
+    expect(store.state.cart.length).toEqual(1)
+    expect(store.state.cart[0].quantity).toEqual(2)
+  })
+  test('that cart has one product with quantity 2 upon dispatch of removal of first product', async () => {
+    await store.dispatch('removeItem', store.state.cart.find(el => el.slug === products[2].slug))
+    expect(store.state.cart.length).toEqual(1)
+    expect(store.state.cart[0].quantity).toEqual(1)
+  })
 })
