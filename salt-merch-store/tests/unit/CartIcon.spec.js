bdd76a40ea4ef38fc236e1fc3a33f4613f6f6685
Alexander Sopov <alexander@alexs-MacBook-Pro.local>
2022-10-28 13:00:46 +0200

    added more tests to carticon

--- salt-merch-store/tests/unit/CartIcon.spec.js
+++ salt-merch-store/tests/unit/CartIcon.spec.js
@@ -2,10 +2,9 @@ import CartIcon from '@/components/Cart/CartIcon'
 import { mount } from '@vue/test-utils'
 import store from '@/store/index'
 import router from '@/router'
+import { nextTick } from 'vue'
 
-beforeEach(() => {
-  jest.clearAllMocks()
-})
+// beforeEach( () => jest.setTimeout(5500) )
 
 const test_item_one = {
   slug: 'test-item',
@@ -31,7 +30,7 @@ const test_item_one = {
 
 
 describe('Testing Cart Icon', () => {
-  it('Checks if CartIcon has zero height by default', async () => {
+  it('Checks if CartIcon is not expanded by default', async () => {
     const wrapper = mount(CartIcon, {
       global: {
         plugins: [store, router],
@@ -45,11 +44,12 @@ describe('Testing Cart Icon', () => {
     const cartIconContainers = wrapper.findAll('[data-testid="expanded-class"]')
     expect(cartIconContainers.length).toEqual(1)
     const cartIconContainer = cartIconContainers[0]
-    let classes = cartIconContainer.classes()
+    const classes = cartIconContainer.classes()
+    console.log('1. classes is', classes)
     expect(classes).not.toContain('expanded')
   })
 
-  it('Checks if CartIcon has more than zero height after adding a product in cart', async () => {
+  it('Checks if CartIcon is expanded after adding a product in cart', async () => {
     const wrapper = mount(CartIcon, {
       global: {
         plugins: [store, router],
@@ -64,9 +64,37 @@ describe('Testing Cart Icon', () => {
     expect(cartIconContainers.length).toEqual(1)
     const cartIconContainer = cartIconContainers[0]
     await store.dispatch('addItem', test_item_one)
-    let classes = cartIconContainer.classes()
+    await nextTick()
+    const classes = cartIconContainer.classes()
+    console.log('2. classes is', classes)
     expect(classes).toContain('expanded')
   })
+
+  it('Checks if CartIcon is not expanded 4 seconds after adding a product in cart', async () => {
+    jest.useFakeTimers();
+    const wrapper = mount(CartIcon, {
+      global: {
+        plugins: [store, router],
+        stubs: {
+          CartList: {
+            template: '<div class="w-full text-left">bunch of stuff</div>'
+          }
+        }
+      }
+    })
+    await store.dispatch('addItem', test_item_one)
+    // await new Promise ( done => {
+    //   setTimeout(() => {
+    jest.runAllTimers();
+    await nextTick()
+    const cartIconContainers = wrapper.findAll('[data-testid="expanded-class"]')
+    expect(cartIconContainers.length).toEqual(1)
+    const cartIconContainer = cartIconContainers[0]
+    const classes = cartIconContainer.classes()
+    console.log('3. classes is', classes)
+    expect(classes).not.toContain('expanded')
+    // done()
+      // }, 5010)
+    // })
+  })
 })
-
-
